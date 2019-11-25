40080a53ff2b6c545950df5aa2faa08cf3634ce5
Unknown <jeanmaxskrebs@gmail.com>
2019-11-25 00:02:08 -0300

    Cria pasta funcão de conexao

---
+++ crud/crudPedidos/conexao.php
@@ -0,0 +1,11 @@
+<?php
+    setlocale(LC_ALL, 'pt_BR','pt_BR.utf-8','pt_BR.utf-8','portuguese');
+    date_default_timezone_set('America/Sao_Paulo');
+    try{
+        $conection = new PDO("pgsql:host=localhost;dbname=site;user=postgres;password=1234");
+        $conection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+    catch(PDOException $ex){
+        echo("Deu erro! <br>". $ex->getMessage());
+    }
+?>
